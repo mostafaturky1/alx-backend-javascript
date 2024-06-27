32965f8c378f39500ea91966615dc4f30d64c973
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-06-27 05:23:42 +0300

    “commit”

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,14 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  const myPromise = new Promise((resolve, reject) => {
     if (denominator === 0) {
-      reject(new Error('cannot divide by 0'));
+      throw Error('cannot divide by 0');
     } else {
-      resolve(numerator / denominator);
+      return numerator / denominator;
     }
-  });
-  try {
-    return myPromise;
-  } catch (resson) {
-    return resson;
-  }
 }
