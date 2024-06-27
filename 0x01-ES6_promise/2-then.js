d151828e1abf54b88af00898a8e311d4fb31ead4
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-06-27 03:36:11 +0300

    “commit”

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,13 @@
-export default function handleResponseFromAPI(promise)
+export default function handleResponseFromAPI(promise){
+  let res = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  res.resolve().then
+}
