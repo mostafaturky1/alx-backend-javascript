3c754b2c0e60c58df80edb191ab268c87746fe9e
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-06-27 04:36:50 +0300

    “commit”

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((result) => {
-      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
