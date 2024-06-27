e7bec320862b9b29a6a076cb39c88492542b7cce
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-06-28 01:54:18 +0300

    “commit”

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,25 +1,9 @@
+import ClassRoom from './0-classroom';
+
 /**
- * Represents a class room.
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
  */
-export default class ClassRoom {
-  /**
-   * Creates a new @see {@link ClassRoom}.
-   * @param {Number} maxStudentsSize - The maximum number of
-   * students in the class.
-   */
-  constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
-  }
-  /**
-   * Initializes and returns an array of ClassRoom objects.
-   * Each ClassRoom object has a maximum capacity of 10 students.
-   * @returns {ClassRoom[]} An array of ClassRoom objects.
-   */
-  initializeRooms() {
-    const rooms = [];
-    for (let i = 1; i <= 5; i++) {
-      rooms.push(new ClassRoom(10));
-    }
-    return rooms;
-  }
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
