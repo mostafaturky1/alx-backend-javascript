ad99edd7c41fc0df9cdde92ed0533c9b8d1217cc
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-06-28 04:19:04 +0300

    “commit”

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,26 +1,20 @@
-/**
- * Represents a building with square footage.
- * @constructor
- * @param {number} sqft - The square footage of the building.
- * @throws Will throw an error if the class is extended and the evacuationWarningMessage method is not overridden.
- */
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
+    this.sqft = sqft;
     if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== "function") {
+      if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
-          "Class extending Building must override evacuationWarningMessage"
+          'Class extending Building must override evacuationWarningMessage',
         );
       }
     }
   }
 
-  /**
-   * Getter for the square footage of the building.
-   * @returns {number} The square footage of the building.
-   */
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
   }
 }
