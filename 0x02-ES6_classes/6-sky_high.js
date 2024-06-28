916837c876f958ad440ce68b59c3192ab970abca
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-06-28 04:28:25 +0300

    “commit”

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,31 +1,16 @@
-/**
- * Represents a sky high building.
- * @extends Building
- */
+import Building from './5-building';
+
 export default class SkyHighBuilding extends Building {
-  /**
-   * Constructs a new SkyHighBuilding instance.
-   * @param {number} sqft - The square footage of the building.
-   * @param {number} floors - The number of floors in the building.
-   */
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
 
-  /**
-   * Getter for the number of floors.
-   * @returns {number} The number of floors in the building.
-   */
   get floors() {
     return this._floors;
   }
 
-  /**
-   * Returns an evacuation warning message.
-   * @returns {string} The evacuation warning message.
-   */
   evacuationWarningMessage() {
-    return "Evacuate slowly the NUMBER_OF_FLOORS floors";
+    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
   }
 }
