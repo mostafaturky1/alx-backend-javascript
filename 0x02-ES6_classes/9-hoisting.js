7198fe3e3821e7db5baf26bb200458c151153e5a
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-06-28 05:26:18 +0300

    “commit”

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -3,18 +3,15 @@ export class HolbertonClass {
     this._year = year;
     this._location = location;
   }
-  
+
   get year() {
     return this._year;
   }
-  
+
   get location() {
     return this._location;
   }
 }
-
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
@@ -26,15 +23,19 @@ export class StudentHolberton {
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
-  
+
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
-  
+
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - \
+${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
