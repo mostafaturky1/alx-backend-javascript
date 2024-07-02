85aab811a3fdc7f4158ce1ff58a746338f78d518
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-02 04:30:03 +0300

    “commit”

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
 export default function getListStudentIds(studentsIds) {
-  if (!Array.isArray(studentsIds)) return [];
+  if (!students instanceof Array) return [];
   return studentsIds.map((studentsIds) => studentsIds.id);
 }
