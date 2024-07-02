cce861ed2118da66fc8b87bcb973529473a450c6
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-02 04:38:02 +0300

    “commit”

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
-export default function getStudentIdsSum(student) {
-  if (!student instanceof Array) return 0;
-  const studentid = student.map((student) => student.id);
+export default function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) return 0;
+  const studentid = students.map((student) => student.id);
   const sum = studentid.reduce((a, b) => { return a + b; }, 0);
   return sum;
 }
