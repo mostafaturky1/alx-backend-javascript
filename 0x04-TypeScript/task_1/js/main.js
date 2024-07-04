001d253ef932e5d92aaa9ba6c46e5f953dc17cf8
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-07-04 03:23:35 +0300

    “commit”

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,8 +1,21 @@
-var director1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-};
-console.log(director1);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.StudentClas = void 0;
+exports.printTeacher = printTeacher;
+function printTeacher(firstName, lastName) {
+    return "".concat(firstName[0], ". ").concat(lastName);
+}
+var StudentClas = /** @class */ (function () {
+    function StudentClas(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClas.prototype.workOnHomework = function () {
+        return "Currently working";
+    };
+    StudentClas.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClas;
+}());
+exports.StudentClas = StudentClas;
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,11 +11,25 @@ export interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
+export function printTeacher(firstName: string, lastName: string) {
+  return `${firstName[0]}. ${lastName}`
+}
+
+export class StudentClas {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return `Currently working`;
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
+
+
